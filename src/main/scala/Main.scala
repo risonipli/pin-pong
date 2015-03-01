add068542e18d884537d6d961c24f0ddb450b825
Artem Prokhorov <risonipli@gmail.com>
2015-03-01 13:46:16 +0300

    supervisor & blow up

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,7 +15,9 @@
  */
 
 import ArgsConfig.Config
+import akka.actor.SupervisorStrategy.{Resume, Restart}
 import akka.actor._
+import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
@@ -24,24 +26,56 @@ case class StartGame(opponent: ActorRef, state: PlayerState)
 case class PinMsg(pin: String = "pin")
 case class PongMsg(pong: String = "pong")
 case class PlayerState(pongs: Int = 0, switchCounter: Int = 0)
+case object BlowUp
+
+class PinPongSuperVisorStrategy extends SupervisorStrategyConfigurator {
+  override def create(): SupervisorStrategy = OneForOneStrategy() {
+    case _: Exception => Resume
+  }
+}
 
 class PinPong(config: Config, name: String) extends Actor {
   var pinCounter = 0
   var pongCounter = 0
   var switchCounter = 0
-  var scheduler: Option[Cancellable] = None
+  var pinSenderSch: Option[Cancellable] = None
+  var blowUpSch: Option[Cancellable] = None
+
+  def startPinSending(opponent: ActorRef) = {
+    if (pinSenderSch.isEmpty)
+      pinSenderSch = Some(this.context.system.scheduler.schedule(0.second, config.pinDelay.millisecond){
+        opponent ! PinMsg()
+      })
+  }
+
+  def cancelPinSending() = {
+    pinSenderSch.map(_.cancel())
+    pinSenderSch = None
+  }
+
+  def startBlowUp() = {
+    if (blowUpSch.isEmpty)
+      blowUpSch = Some(context.system.scheduler.schedule(config.failDelay.millisecond, config.failDelay.millisecond) {
+        self ! BlowUp
+      })
+  }
+
+  def cancelBlowUp() = {
+    blowUpSch.map(_.cancel())
+    blowUpSch = None
+  }
+
   def receive = {
     case StartGame(opponent, state) =>
       pongCounter = state.pongs
       switchCounter = state.switchCounter
-      scheduler = Some(this.context.system.scheduler.schedule(0.second, config.pinDelay.millisecond){
-        opponent ! PinMsg()
-      })
+      startPinSending(opponent)
 
     case PinMsg(pin) =>
       println(s"$name received: $pin")
       pinCounter += 1
-
+      startBlowUp()
+      
       if (pinCounter % config.pinNumber == 0) {
         sender ! PongMsg()
       }
@@ -51,13 +85,16 @@ class PinPong(config: Config, name: String) extends Actor {
       println(s"$name received: $pong\n")
       if (pongCounter != 0 && pongCounter % config.switch == 0) {
         switchCounter += 1
+        cancelPinSending()
         if (switchCounter == config.set) this.context.system.shutdown()
         else {
-          scheduler.map(s => s.cancel())
+          pinSenderSch.map(s => s.cancel())
           sender ! StartGame(self, PlayerState(pongCounter, switchCounter))
           println("=========SWITCH===========")
         }
       }
+
+    case BlowUp => throw new Exception(s"Player $name blowed up with $pinCounter")
   }
 }
 
@@ -69,7 +106,8 @@ object Main {
           println("Опция --fail-delay должна быть больше чем опция --pin-delay")
           System.exit(1)
         }
-        val system = ActorSystem("pinpong")
+        val akkaConfig = ConfigFactory.load()
+        val system = ActorSystem("pinpong", akkaConfig)
         val player1 = system.actorOf(Props(classOf[PinPong], config, "player1"))
         val player2 = system.actorOf(Props(classOf[PinPong], config, "player2"))
 
